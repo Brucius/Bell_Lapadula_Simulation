f046a76b5a71be3cb7e45f6d91a63fa804183e16
ominbruce <brucetheflame@outlook.com>
2019-07-16 17:57:56 +0800

    copyright addition and minor comment change

--- main.cpp
+++ main.cpp
@@ -1,3 +1,27 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2019 OAKER MIN
+
+//  Permission is hereby granted, free of charge, to any person obtaining a
+//  copy of this software and associated documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//  and/or sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//  DEALINGS IN THE SOFTWARE.
+
+/* code */
+
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -47,7 +71,7 @@ void createFile();
 void readFile(string&);
 void promptExit();
 
-/*************************************************  START OF MAIN FILE *************************************************/
+/*************************************************  START OF MAIN FUNCTION *************************************************/
 int main(int argc, char *argv[])
 {
   if (argc == 2 && string(argv[1]) == "-i")
@@ -62,7 +86,7 @@ int main(int argc, char *argv[])
   }
   return 0;
 }
-/*************************************************  END OF MAIN FILE *************************************************/
+/*************************************************  END OF MAIN FUNCTION *************************************************/
 
 /************************************************* Function Implementations *************************************************/
 void createAccount()
